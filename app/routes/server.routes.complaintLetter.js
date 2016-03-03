844cdf281d1ebe4c6131cbf7941bca89c374418d
Meegan <matt.l.egan@gmail.com>
2016-03-03 15:34:29 -0500

    okay, one more time..

--- app/routes/server.routes.complaintLetter.js
+++ app/routes/server.routes.complaintLetter.js
@@ -9,8 +9,8 @@ var express    = require('express'),        // call express
 // middleware to use for all requests
 router.use(function(req, res, next) {
 	// get CORS to work correctly
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   
   next(); // make sure we go to the next routes and don't stop here
 });
