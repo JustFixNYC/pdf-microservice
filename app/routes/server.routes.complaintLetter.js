5a9c0763b01d9935a8926c291f0ce964b3310839
Meegan <matt.l.egan@gmail.com>
2016-03-03 15:46:04 -0500

    dunno

--- app/routes/server.routes.complaintLetter.js
+++ app/routes/server.routes.complaintLetter.js
@@ -8,9 +8,7 @@ var express    = require('express'),        // call express
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-	// get CORS to work correctly
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	
   
   next(); // make sure we go to the next routes and don't stop here
 });
--- config/init.js
+++ config/init.js
@@ -22,7 +22,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  next();
 
   next();
 })
