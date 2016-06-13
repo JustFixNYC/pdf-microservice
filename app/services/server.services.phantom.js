5a47cd9b6a6103ffbf0d7e8e230ed2f9886c60b8
meegan <matt.l.egan@gmail.com>
2016-06-13 17:05:09 -0400

    debugging

--- app/services/server.services.phantom.js
+++ app/services/server.services.phantom.js
@@ -1,46 +1,5 @@
 var q = require('q'), // Shoulda used native promises, but w/e
 		phantom = require('phantom');
-/*
-// Determine if accompanying info was sent from controller correctly
-if(args.length === 1) {
-	throw new Error('no args passed! Please make sure the submission was sent correctly');
-} else {
-	page.content = args[1];
-}
-
-// Format paper so PDF comes out purrty
-page.paperSize = {
-  format: 'A4',
-  orientation: 'portrait',
-  margin: '1in'
-};
-
-// Once we correctly load our PDF as a webview, then this fires
-page.onLoadFinished = function () {
-	var title = 'temp/file' + Math.floor(Math.random() * 1000) + 1 + '.pdf';
-	page.render(title);
-	system.stdout.write(title);
-	phantom.exit();
-}
-
-// Error tracking
-page.onResourceError = function(resourceError) {
-    system.stderr.writeLine('= onResourceError()');
-    system.stderr.writeLine('  - unable to load url: "' + resourceError.url + '"');
-    system.stderr.writeLine('  - error code: ' + resourceError.errorCode + ', description: ' + resourceError.errorString );
-};
-
-page.onError = function(msg, trace) {
-    system.stderr.writeLine('= onError()');
-    var msgStack = ['  ERROR: ' + msg];
-    if (trace) {
-        msgStack.push('  TRACE:');
-        trace.forEach(function(t) {
-            msgStack.push('    -> ' + t.file + ': ' + t.line );
-        });
-    }
-    system.stderr.writeLine(msgStack.join('\n'));
-};*/
 
 // NEW CODE
 var session;
@@ -48,13 +7,17 @@ var session;
 var createPhantomSession = function(args) {
 	var def = q.defer();
   if (session) {
-    // console.log('session exists LOL');
+    console.log('session exists LOL');
     def.resolve(renderPdf(session, args));
   } else {
-    // console.log('new session');
-    session = phantom.create().then(function(_session){
+    console.log('new session created');
+    phantom.create().then(function(_session){
+    	console.log('session created');
     	session = _session;
     	def.resolve(renderPdf(session, args));
+    }, function(error) {
+    	console.log(error);
+    	def.reject(error);
     });
   }
 
@@ -64,11 +27,23 @@ var createPhantomSession = function(args) {
 var renderPdf = function(session, args) {
   var page;
   var deferred = q.defer();
+  console.log('renderPDF called');
 
   try {
-
     session.createPage().then(function(_page) {
+    	console.log('page created')
       page = _page;
+
+			page.on('onError', function(err){
+				console.log('error');
+				console.log(err);
+				deferred.reject(err);
+			});
+
+			page.on('onResourceError', function(resourceError) {
+			  console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+			  console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+			});
 
       var file = 'temp/file' + Math.floor(Math.random() * 1000) + 1 + '.pdf';
 
@@ -80,21 +55,21 @@ var renderPdf = function(session, args) {
 			  margin: '1in'
 			});
 
-			page.on('onError', function(err){
-				console.log('error');
-				console.log(err);
-				deferred.reject(err);
-			});
 			page.on('onLoadFinished', function(){
+				console.log('loading finished');
 	      page.render(file).then(function(){
+	      	console.log('finally done building the PDF');
 		      page.close();
 		      page = null;
 		      deferred.resolve(file);
 	      });
 			})
+    }, function(err) {
+    	console.log(err);
     });
 
   } catch(e) {
+  	console.log('WHAT');
     try {
       if (page != null) {
         page.close(); // try close the page in case it opened but never rendered a pdf due to other issues
